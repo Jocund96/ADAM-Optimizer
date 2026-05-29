09269a19d101962923f691ddce7b03649ab51acc
Usama Renders <114694792+usama-469@users.noreply.github.com>
2026-05-29 17:01:53 +0200

    pdf notes

--- adam_bauhaus.m
+++ adam_bauhaus.m
@@ -7,7 +7,14 @@
 % max_iter  : Maximum number of iterations
 
 % --- INITIALIZATION ---
-alpha = 0.001;
-beta1 = 0.9;
+alpha = 0.001; % Learning rate
+beta1 = 0.9;  % Exponential Decay rates
 beta2 = 0.999;
 epsilon = 1e-8;
+
+% Initialize variables for the optimizer
+w = w0; % Current weight
+m = zeros(size(w0)); % First moment vector  m_0
+v = zeros(size(w0)); % Second moment vector  v_0
+
+%-----OPTIMIZATION LOOP
